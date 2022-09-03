106ec9f783457d1d27b1a77a40450eb5836b47e0
MahmdulHasan <mahmudul92hasan@gmail.com>
2022-09-03 21:42:52 +0600

    create angular component & user it app component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
