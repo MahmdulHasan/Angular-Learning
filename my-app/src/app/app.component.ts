e2b7b7db40171acd1bb9c9146b164de7b873377a
MahmdulHasan <mahmudul92hasan@gmail.com>
2022-09-23 22:42:21 +0600

    hands on example

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  counterValue = 0;
+
+  increaseCounter(){
+    this.counterValue = this.counterValue + 1; 
+  }
+
+  decreaseCounter(){
+    this.counterValue = this.counterValue - 1; 
+  }
 }
